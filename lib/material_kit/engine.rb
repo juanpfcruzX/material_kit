9d80c0a22a3e9fed929306f6e5fadc5f5cf8c996
Tim <timshingyu@gmail.com>
2016-03-14 18:38:37 +0800

    Added sass and modified application.css

--- lib/material_kit/engine.rb
+++ lib/material_kit/engine.rb
@@ -1,4 +1,15 @@
 module MaterialKit
   class Engine < ::Rails::Engine
+    initializer 'material_kit.setup' do |app|
+      app.config.assets.precompile += %w( material_kit/avatar.jpg material_kit/christian.jpg material_kit/kendall.jpg )
+    end
+
+    initializer 'material_kit.sass' do |app|
+      # Set the sass load paths
+      app.config.sass.load_paths << File.join(config.root, "lib", "sass")
+
+      # Set the add material_kit/lib/sass to assets path
+      app.config.assets.paths << File.join(config.root, "lib", "sass")
+    end
   end
 end
